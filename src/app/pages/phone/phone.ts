6d55e3268c76b31ca8a6f95c186ed903e9ea133e
Igor Smugalov <smugalovigor@gmail.com>
2025-11-13 18:52:48 +0300

    ✨ add base structure

---
+++ src/app/pages/phone/phone.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'sip-phone-page',
+  imports: [],
+  templateUrl: './phone.html',
+  styleUrl: './phone.scss',
+})
+export class SipPhonePage {
+
+}
